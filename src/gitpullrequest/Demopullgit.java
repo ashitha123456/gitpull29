53642d659e5e8430f2002aa449853d24de035b21
madhu2810 <madhurids2892@gmail.com>
2019-07-29 14:46:24 +0530

    commit gitpull

--- src/gitpullrequest/Demopullgit.java
+++ src/gitpullrequest/Demopullgit.java
@@ -1,4 +1,5 @@
 package gitpullrequest;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,28 +8,22 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class Demopullgit {
 
 	@Test
-	public void sample() throws InterruptedException
-	{
+	public void sample() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\new\\src\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.bluestone.com/");
+		driver.manage().window().maximize();
+		Actions act = new Actions(driver);
 
-			
-		        System.setProperty("webdriver.chrome.driver","C:\\selenium\\new\\src\\chromedriver.exe");
-				WebDriver driver=new ChromeDriver();
-				driver.get("https://www.bluestone.com/");
-				driver.manage().window().maximize();
-				Actions act=new Actions(driver);
-				WebElement ele=driver.findElement(By.xpath("//li[@class='menuparent gold-coins']"));
-			    act.moveToElement(ele).build().perform();
-			    driver.findElement(By.xpath("//span[@data-p='gold-coins-weight-20gms,m']")).click();
-				Thread.sleep(2000);
-				Assert.assertEquals(driver.getTitle(),"20 gram 24 KT Gold Coin | BlueStone.com");
-			}
-
-		
-
-
-
+		WebElement ele = driver.findElement(By.xpath("//li[@class='menuparent gold-coins']"));
+		act.moveToElement(ele).build().perform();
+		driver.findElement(By.xpath("//span[@data-p='gold-coins-weight-20gms,m']")).click();
+		Thread.sleep(2000);
+		Assert.assertEquals(driver.getTitle(), "20 gram 24 KT Gold Coin | BlueStone.com");
+		driver.close();
 	}
+
+}
